bdad95f14806c36dd07bfee47530b667e4dd8f29
Ying Shu (Wicresoft) <v-yingshu@microsoft.com>
2022-05-05 14:30:21 +0800

    update

--- internal/services/monitor/monitor_scheduled_query_rules_log_resource_test.go
+++ internal/services/monitor/monitor_scheduled_query_rules_log_resource_test.go
@@ -146,34 +146,34 @@ resource "azurerm_resource_group" "test" {
 
 resource "azurerm_application_insights" "test" {
   name                = "acctestAppInsights-%[1]d"
-  location            = "azurerm_resource_group.test.location"
-  resource_group_name = "azurerm_resource_group.test.name"
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
   application_type    = "web"
 }
 
 resource "azurerm_log_analytics_workspace" "test" {
   name                = "acctestWorkspace-%[1]d"
-  location            = "azurerm_resource_group.test.location"
-  resource_group_name = "azurerm_resource_group.test.name"
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
   sku                 = "PerGB2018"
   retention_in_days   = 30
 }
 
 resource "azurerm_monitor_action_group" "test" {
   name                = "acctestActionGroup-%[1]d"
-  resource_group_name = "azurerm_resource_group.test.name"
+  resource_group_name = azurerm_resource_group.test.name
   short_name          = "acctestag"
 }
 
 resource "azurerm_monitor_scheduled_query_rules_log" "test" {
   name                = "acctestsqr-%[1]d"
-  resource_group_name = "azurerm_resource_group.test.name"
-  location            = "azurerm_resource_group.test.location"
+  resource_group_name = azurerm_resource_group.test.name
+  location            = azurerm_resource_group.test.location
   description         = "test log to metric action"
   enabled             = true
 
-  data_source_id          = "azurerm_log_analytics_workspace.test.id"
-  authorized_resource_ids = ["azurerm_application_insights.test.id", "azurerm_log_analytics_workspace.test.id"]
+  data_source_id          = azurerm_log_analytics_workspace.test.id
+  authorized_resource_ids = [azurerm_application_insights.test.id, azurerm_log_analytics_workspace.test.id]
 
   criteria {
     metric_name = "Average_%% Idle Time"
@@ -190,20 +190,20 @@ resource "azurerm_monitor_scheduled_query_rules_log" "test" {
 
 resource "azurerm_monitor_metric_alert" "test" {
   name                = "acctestmal-%[1]d"
-  resource_group_name = "azurerm_resource_group.test.name"
-  scopes              = ["azurerm_log_analytics_workspace.test.id"]
+  resource_group_name = azurerm_resource_group.test.name
+  scopes              = [azurerm_log_analytics_workspace.test.id]
   description         = "Action will be triggered when Average %% Idle Time is less than 10."
 
   criteria {
     metric_namespace = "Microsoft.OperationalInsights/workspaces"
-    metric_name      = "azurerm_monitor_scheduled_query_rules_log.test.criteria[0].metric_name"
+    metric_name      = azurerm_monitor_scheduled_query_rules_log.test.criteria[0].metric_name
     aggregation      = "Average"
     operator         = "LessThan"
     threshold        = 10
   }
 
   action {
-    action_group_id = "azurerm_monitor_action_group.test.id"
+    action_group_id = azurerm_monitor_action_group.test.id
   }
 }
 `, data.RandomInteger, data.Locations.Primary)
